eadb9d7790c35fbafe58d21840faa56332d6748c
Qais Yousef <qyousef@layalina.io>
2023-12-09 01:17:58 +0000

    FROMLIST: sched/fair: Generalize misfit lb by adding a misfit reason
    MISFIT_PERF is what is currently implemented. It indicates that the task
    requires moving to a more performant CPU.
    
    Guard misfit handling in find_busiest_queue and update_sd_pick_busiest
    with MISFIT_PERF. They explicitly assume this type of misfit
    
    This generalizes misfit lb to allow for more types of misfits to be
    added. Like MISFIT_POWER to help uclamp_max being more effective, and
    MISFIT_LATENCY to help latency sensitive tasks to be spread in
    oversubscribe conditions.
    
    Bug: 340190651
    Link: https://lore.kernel.org/lkml/20231209011759.398021-3-qyousef@layalina.io/
    Change-Id: Id798e8936dddcf368c2956bad708e99087c1076f
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Qais Yousef <qyousef@google.com>
    [jstultz: Added lore link, small build fix from bad cherry-pick,
     whitespace indention fixup]
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5008,7 +5008,8 @@ static inline int task_fits_cpu(struct task_struct *p, int cpu)
 	return (util_fits_cpu(util, uclamp_min, uclamp_max, cpu) > 0);
 }
 
-static inline int is_misfit_task(struct task_struct *p, struct rq *rq)
+static inline int is_misfit_task(struct task_struct *p, struct rq *rq,
+				 misfit_reason_t *reason)
 {
 	if (!p || p->nr_cpus_allowed == 1)
 		return 0;
@@ -5016,19 +5017,24 @@ static inline int is_misfit_task(struct task_struct *p, struct rq *rq)
 	if (task_fits_cpu(p, cpu_of(rq)))
 		return 0;
 
+	if (reason)
+		*reason = MISFIT_PERF;
+
 	return 1;
 }
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
 	bool need_update = true;
+	misfit_reason_t reason;
 
 	trace_android_rvh_update_misfit_status(p, rq, &need_update);
 	if (!sched_asym_cpucap_active() || !need_update)
 		return;
 
-	if (!is_misfit_task(p, rq)) {
+	if (!is_misfit_task(p, rq, &reason)) {
 		rq->misfit_task_load = 0;
+		rq->misfit_reason = -1;
 		return;
 	}
 
@@ -5037,6 +5043,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 	 * task_h_load() returns 0.
 	 */
 	rq->misfit_task_load = max_t(unsigned long, task_h_load(p), 1);
+	rq->misfit_reason = reason;
 }
 EXPORT_SYMBOL_GPL(update_misfit_status);
 
@@ -9126,7 +9133,7 @@ static int detach_tasks(struct lb_env *env)
 
 		case migrate_misfit:
 			/* This is not a misfit task */
-			if (!is_misfit_task(p, cpu_rq(env->src_cpu)))
+			if (!is_misfit_task(p, cpu_rq(env->src_cpu), NULL))
 				goto next;
 
 			env->imbalance = 0;
@@ -9443,6 +9450,7 @@ struct sg_lb_stats {
 	unsigned int group_asym_packing; /* Tasks should be moved to preferred CPU */
 	unsigned int group_smt_balance;  /* Task on busy SMT be moved */
 	unsigned long group_misfit_task_load; /* A CPU has a task too big for its capacity */
+	misfit_reason_t group_misfit_reason;
 #ifdef CONFIG_NUMA_BALANCING
 	unsigned int nr_numa_running;
 	unsigned int nr_preferred_running;
@@ -9919,6 +9927,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			/* Check for a misfit task on the cpu */
 			if (sgs->group_misfit_task_load < rq->misfit_task_load) {
 				sgs->group_misfit_task_load = rq->misfit_task_load;
+				sgs->group_misfit_reason = rq->misfit_reason;
 				*sg_status |= SG_OVERLOAD;
 			}
 		} else if ((env->idle != CPU_NOT_IDLE) &&
@@ -9984,6 +9993,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	 */
 	if ((env->sd->flags & SD_ASYM_CPUCAPACITY) &&
 	    (sgs->group_type == group_misfit_task) &&
+	    (sgs->group_misfit_reason == MISFIT_PERF) &&
 	    (!capacity_greater(capacity_of(env->dst_cpu), sg->sgc->max_capacity) ||
 	     sds->local_stat.group_type != group_has_spare))
 		return false;
@@ -10197,6 +10207,7 @@ static inline void update_sg_wakeup_stats(struct sched_domain *sd,
 
 	for_each_cpu(i, sched_group_span(group)) {
 		struct rq *rq = cpu_rq(i);
+		misfit_reason_t reason;
 		unsigned int local;
 
 		sgs->group_load += cpu_load_without(rq, p);
@@ -10216,9 +10227,16 @@ static inline void update_sg_wakeup_stats(struct sched_domain *sd,
 
 		/* Check if task fits in the CPU */
 		if (sd->flags & SD_ASYM_CPUCAPACITY &&
-		    sgs->group_misfit_task_load &&
-		    !is_misfit_task(p, rq))
-			sgs->group_misfit_task_load = 0;
+		    sgs->group_misfit_task_load) {
+			if (!is_misfit_task(p, rq, &reason)) {
+				sgs->group_misfit_task_load = 0;
+				sgs->group_misfit_reason = -1;
+			} else {
+				sgs->group_misfit_task_load =
+					max_t(unsigned long, task_h_load(p), 1);
+				sgs->group_misfit_reason = reason;
+			}
+		}
 
 	}
 
@@ -11018,6 +11036,7 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 * average load.
 		 */
 		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
+		    rq->misfit_reason == MISFIT_PERF &&
 		    !capacity_greater(capacity_of(env->dst_cpu), capacity) &&
 		    nr_running == 1)
 			continue;
