333168af5e2681bff150d56d3e3da3ab19fde7d6
Nitin Rawat <quic_nitirawa@quicinc.com>
2025-02-18 19:52:20 -0800

    scsi: ufs: qcom: Fix incorrect pm levels initialization
    With the upstream 'commit <ac216ffa697c>
    ("scsi: ufs: core: Honor runtime/system PM levels
    if set by host controller drivers")', the platform driver
    initialization of spm/rpm levels to -1 would become invalid
    setting and cause problems when the system tries to enter lpm.
    Remove the redundant function ufs_qcom_parse_pm_level().
    Override the spm/rpm levels only if the platform's DT provides
    the valid spm/rpm-level entries. Otherwise, leave the default
    settings from the core driver unchanged.
    
    Additionally, remove the is_dt_pm_level_read handling in the
    code as it is no longer necessary, and preserve the
    set_ds_spm_level from the existing code for certain targets.
    
    Change-Id: I85ade8a026994e0a886ac91b31ed8b7f87e5e78a
    Signed-off-by: Bao D. Nguyen <quic_nguyenb@quicinc.com>
    Signed-off-by: Nitin Rawat <quic_nitirawa@quicinc.com>
    Signed-off-by: Manish Pandey <quic_mapa@quicinc.com>

--- drivers/ufs/host/ufs-qcom.c
+++ drivers/ufs/host/ufs-qcom.c
@@ -2500,7 +2500,7 @@ ufshcd_is_valid_pm_lvl(enum ufs_pm_level lvl)
 	return lvl >= 0 && lvl < UFS_PM_LVL_MAX;
 }
 
-static void ufshcd_parse_pm_levels(struct ufs_hba *hba)
+static void ufs_qcom_parse_pm_levels(struct ufs_hba *hba)
 {
 	struct device *dev = hba->dev;
 	struct device_node *np = dev->of_node;
@@ -2510,16 +2510,15 @@ static void ufshcd_parse_pm_levels(struct ufs_hba *hba)
 	if (!np)
 		return;
 
-	if (host->is_dt_pm_level_read)
-		return;
-
 	if (!of_property_read_u32(np, "rpm-level", &rpm_lvl) &&
 		ufshcd_is_valid_pm_lvl(rpm_lvl))
 		hba->rpm_lvl = rpm_lvl;
 	if (!of_property_read_u32(np, "spm-level", &spm_lvl) &&
 		ufshcd_is_valid_pm_lvl(spm_lvl))
 		hba->spm_lvl = spm_lvl;
-	host->is_dt_pm_level_read = true;
+	if (of_property_read_bool(np, "set-ds-spm-level"))
+		host->set_ds_spm_level = true;
+
 }
 
 static void ufs_qcom_override_pa_tx_hsg1_sync_len(struct ufs_hba *hba)
@@ -2561,8 +2560,6 @@ static int ufs_qcom_apply_dev_quirks(struct ufs_hba *hba)
 
 	if (hba->dev_quirks & UFS_DEVICE_QUIRK_PA_TX_HSG1_SYNC_LENGTH)
 		ufs_qcom_override_pa_tx_hsg1_sync_len(hba);
-
-	ufshcd_parse_pm_levels(hba);
 
 	if (hba->dev_info.wmanufacturerid == UFS_VENDOR_MICRON)
 		hba->dev_quirks |= UFS_DEVICE_QUIRK_DELAY_BEFORE_LPM;
@@ -3235,25 +3232,6 @@ static void ufs_qcom_parse_irq_affinity(struct ufs_hba *hba)
 		host->irq_affinity_support = true;
 }
 
-static void ufs_qcom_parse_pm_level(struct ufs_hba *hba)
-{
-	struct device *dev = hba->dev;
-	struct device_node *np = dev->of_node;
-	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
-
-	if (np) {
-		if (of_property_read_u32(np, "rpm-level",
-					 &hba->rpm_lvl))
-			hba->rpm_lvl = -1;
-		if (of_property_read_u32(np, "spm-level",
-					 &hba->spm_lvl))
-			hba->spm_lvl = -1;
-
-		if (of_property_read_bool(np, "set-ds-spm-level"))
-			host->set_ds_spm_level = true;
-	}
-}
-
 /* Returns the max mitigation level supported */
 static int ufs_qcom_get_max_therm_state(struct thermal_cooling_device *tcd,
 				  unsigned long *data)
@@ -3901,7 +3879,7 @@ static int ufs_qcom_init(struct ufs_hba *hba)
 	if (err)
 		goto out_disable_parent_vreg;
 
-	ufs_qcom_parse_pm_level(hba);
+	ufs_qcom_parse_pm_levels(hba);
 	ufs_qcom_parse_g4_workaround_flag(host);
 	ufs_qcom_parse_lpm(host);
 	if (host->disable_lpm)
