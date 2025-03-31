0a443cf98d21d0340fc11d8c18a1f6203b5aa288
John Galt <johngaltfirstrun@gmail.com>
2025-03-31 12:05:50 -0400

    qcom minidump: fix build

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -725,7 +725,7 @@ static void md_dump_task_info(struct task_struct *task, char *status,
 	se = &task->se;
 	if (task == curr) {
 		seq_buf_printf(md_runq_seq_buf,
-			       "[status: curr] pid: %d preempt: %#x\n",
+			       "[status: curr] pid: %d preempt: %#llx\n",
 			       task_pid_nr(task),
 			       task->thread_info.preempt_count);
 		return;
@@ -797,8 +797,6 @@ static void md_dump_cfs_rq(struct cfs_rq *cfs, struct task_struct *curr)
 
 	md_dump_cgroup_state("curr", cfs->curr, curr);
 	md_dump_cgroup_state("next", cfs->next, curr);
-	md_dump_cgroup_state("last", cfs->last, curr);
-	md_dump_cgroup_state("skip", cfs->skip, curr);
 	md_rb_walk_cfs(rb_root_cached_p, curr);
 }
 
@@ -872,7 +870,7 @@ static void md_dump_next_event(void)
 	for_each_possible_cpu(cpu) {
 		event_dev = per_cpu(device_dump->evtdev, cpu);
 		if (event_dev)
-			pr_emerg("CPU%d next event is %ld\n", cpu,
+			pr_emerg("CPU%d next event is %lld\n", cpu,
 				event_dev->next_event);
 		else
 			pr_emerg("CPU%d next event is not available\n", cpu);
