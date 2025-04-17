76dd6a53e0cdecec39fa9d671ce8dbc0612854fe
Xuewen Yan <xuewen.yan@unisoc.com>
2025-04-17 12:34:57 +0800

    sched/uclamp: Align uclamp and util_est and call before freq update
    The commit dfa0a574cbc47 ("sched/uclamg: Handle delayed dequeue")
    has add the sched_delayed check to prevent double uclamp_dec/inc.
    However, it put the uclamp_rq_inc() after enqueue_task().
    This may lead to the following issues:
    When a task with uclamp goes through enqueue_task() and could trigger
    cpufreq update, its uclamp won't even be considered in the cpufreq
    update. It is only after enqueue will the uclamp be added to rq
    buckets, and cpufreq will only pick it up at the next update.
    This could cause a delay in frequency updating. It may affect
    the performance(uclamp_min > 0) or power(uclamp_max < 1024).
    
    So, just like util_est, put the uclamp_rq_inc() before enqueue_task().
    And as for the sched_delayed_task, same as util_est, using the
    sched_delayed flag to prevent inc the sched_delayed_task's uclamp,
    using the ENQUEUE_DELAYED flag to allow inc the sched_delayed_task's uclamp
    which is being woken up.
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20250417043457.10632-3-xuewen.yan@unisoc.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1706,7 +1706,7 @@ static inline void uclamp_rq_dec_id(struct rq *rq, struct task_struct *p,
 	}
 }
 
-static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p)
+static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p, int flags)
 {
 	enum uclamp_id clamp_id;
 
@@ -1722,7 +1722,8 @@ static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p)
 	if (unlikely(!p->sched_class->uclamp_enabled))
 		return;
 
-	if (p->se.sched_delayed)
+	/* Only inc the delayed task which being woken up. */
+	if (p->se.sched_delayed && !(flags & ENQUEUE_DELAYED))
 		return;
 
 	for_each_clamp_id(clamp_id)
@@ -2111,7 +2112,7 @@ static void __init init_uclamp(void)
 }
 
 #else /* CONFIG_UCLAMP_TASK */
-static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p) { }
+static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p, int flags) { }
 static inline void uclamp_rq_dec(struct rq *rq, struct task_struct *p) { }
 static inline int uclamp_validate(struct task_struct *p,
 				  const struct sched_attr *attr, bool user)
@@ -2154,12 +2155,14 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	if (!(flags & ENQUEUE_NOCLOCK))
 		update_rq_clock(rq);
 
-	p->sched_class->enqueue_task(rq, p, flags);
 	/*
-	 * Must be after ->enqueue_task() because ENQUEUE_DELAYED can clear
-	 * ->sched_delayed.
+	 * Can be before ->enqueue_task() because uclamp considers the
+	 * ENQUEUE_DELAYED task before its ->sched_delayed gets cleared
+	 * in ->enqueue_task().
 	 */
-	uclamp_rq_inc(rq, p);
+	uclamp_rq_inc(rq, p, flags);
+
+	p->sched_class->enqueue_task(rq, p, flags);
 
 	psi_enqueue(p, flags);
 
