92b6c7d51be30084877dedb31428d4fa1b0de7a4
Peter Zijlstra <peterz@infradead.org>
2025-11-07 17:01:31 +0100

    sched/fair: Proportional newidle balance
    Add a randomized algorithm that runs newidle balancing proportional to
    its success rate.
    
    This improves schbench significantly:
    
     6.18-rc4:			2.22 Mrps/s
     6.18-rc4+revert:		2.04 Mrps/s
     6.18-rc4+revert+random:	2.18 Mrps/S
    
    Conversely, per Adam Li this affects SpecJBB slightly, reducing it by 1%:
    
     6.17:			-6%
     6.17+revert:		 0%
     6.17+revert+random:	-1%
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Chris Mason <clm@meta.com>
    Link: https://lkml.kernel.org/r/6825c50d-7fa7-45d8-9b81-c6e7e25738e2@meta.com
    Link: https://patch.msgid.link/20251107161739.770122091@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -125,6 +125,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_stat_blocked);
 #endif
 
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
+DEFINE_PER_CPU(struct rnd_state, sched_rnd_state);
 EXPORT_SYMBOL_GPL(runqueues);
 
 #ifdef CONFIG_SCHED_DEBUG
@@ -10074,6 +10075,8 @@ int sched_cpu_dying(unsigned int cpu)
 void __init sched_init_smp(void)
 {
 	sched_init_numa(NUMA_NO_NODE);
+
+	prandom_init_once(&sched_rnd_state);
 
 	/*
 	 * There's no userspace yet to cause hotplug operations; hence all the
