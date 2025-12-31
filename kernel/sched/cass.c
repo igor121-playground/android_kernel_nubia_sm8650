f00371891fad53564841f326365b7df2943fa397
John Galt <johngaltfirstrun@gmail.com>
2025-12-31 10:20:53 -0500

    sched/cass: pick idle cpu later, treating as tie-breaker

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -153,16 +153,16 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	if (cass_cmp(b->util, a->util))
 		goto done;
 
-	/* Prefer the CPU that is idle (only relevant for uclamped tasks) */
-	if (cass_cmp(!!a->exit_lat, !!b->exit_lat))
-		goto done;
-
 	/* Prefer the current CPU for sync wakes */
 	if (sync && (cass_eq(a->cpu, this_cpu) || !cass_cmp(b->cpu, this_cpu)))
 		goto done;
 
 	/* Prefer the CPU with higher capacity */
 	if (cass_cmp(a->cap, b->cap))
+		goto done;
+
+	/* Prefer the CPU that is idle (tie-breaker) */
+	if (cass_cmp(!!a->exit_lat, !!b->exit_lat))
 		goto done;
 
 	/* Prefer the CPU with lower idle exit latency */
