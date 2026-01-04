63807d13ff4d04f157adf597bec89b1f37346a77
John Galt <johngaltfirstrun@gmail.com>
2026-01-04 09:31:46 -0500

    Revert "sched/fair: Block delayed tasks on throttled hierarchy during dequeue"
    This reverts commit d2bcb8773211667d532dbb648385916e26497abd.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6868,7 +6868,6 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 	int h_nr_runnable = 0;
 	struct cfs_rq *cfs_rq;
 	u64 slice = 0;
-	int ret = 0;
 
 	if (entity_is_task(se)) {
 		p = task_of(se);
@@ -6898,7 +6897,7 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 
 		/* end evaluation on encountering a throttled cfs_rq */
 		if (cfs_rq_throttled(cfs_rq))
-			goto out;
+			return 0;
 
 		/* Don't dequeue parent if it has other entities besides us */
 		if (cfs_rq->load.weight) {
@@ -6940,7 +6939,7 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 
 		/* end evaluation on encountering a throttled cfs_rq */
 		if (cfs_rq_throttled(cfs_rq))
-			goto out;
+			return 0;
 	}
 
 	sub_nr_running(rq, h_nr_queued);
@@ -6949,8 +6948,6 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 	if (unlikely(!was_sched_idle && sched_idle_rq(rq)))
 		rq->next_balance = jiffies;
 
-	ret = 1;
-out:
 	if (p && task_delayed) {
 		SCHED_WARN_ON(!task_sleep);
 		SCHED_WARN_ON(p->on_rq != 1);
@@ -6966,7 +6963,7 @@ out:
 		__block_task(rq, p);
 	}
 
-	return ret;
+	return 1;
 }
 
 /*
